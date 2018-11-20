97c456faa611c5d68d9556141c443032ae74aa3e
olgabot <olga.botvinnik@gmail.com>
2018-11-20 15:27:04 -0800

    add functions to deal with depedency links

--- setup.py
+++ setup.py
@@ -21,6 +21,58 @@ test_requirements = [
     'pytest', 'coverage'
 ]
 
+
+# Cribbed from https://github.com/pypa/setuptools/issues/1052
+def get_requirements(remove_links=True):
+    """
+    lists the requirements to install.
+    """
+    requirements = []
+    try:
+        with open('requirements.txt') as f:
+            requirements = f.read().splitlines()
+    except Exception as ex:
+        with open('aguamenti.egg-info\requires.txt') as f:
+            requirements = f.read().splitlines()
+    if remove_links:
+        for requirement in requirements:
+            # git repository url.
+            if requirement.startswith("git+"):
+                requirements.remove(requirement)
+            # subversion repository url.
+            if requirement.startswith("svn+"):
+                requirements.remove(requirement)
+            # mercurial repository url.
+            if requirement.startswith("hg+"):
+                requirements.remove(requirement)
+    return requirements
+
+
+def get_links():
+    """
+    gets URL Dependency links.
+    """
+    links_list = get_requirements(remove_links=False)
+    for link in links_list:
+        keep_link = False
+        already_removed = False
+        # git repository url.
+        if not link.startswith("git+"):
+            if not link.startswith("svn+"):
+                if not link.startswith("hg+"):
+                    links_list.remove(link)
+                    already_removed = True
+                else:
+                    keep_link = True
+                if not keep_link and not already_removed:
+                    links_list.remove(link)
+                    already_removed = True
+            else:
+                keep_link = True
+            if not keep_link and not already_removed:
+                links_list.remove(link)
+    return links_list
+
 setup(
     name='aguamenti',
     version='0.1.0',
@@ -38,7 +90,8 @@ setup(
     package_dir={'aguamenti':
                  'aguamenti'},
     include_package_data=True,
-    install_requires=requirements,
+    install_requires=get_requirements(),
+    dependency_links=get_links(),
     license="BSD",
     zip_safe=False,
     keywords='aguamenti',
