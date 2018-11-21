1c530435c6cdecca11f6e62ba1e498ede8de3632
olgabot <olga.botvinnik@gmail.com>
2018-11-21 12:47:19 -0800

    Get 98% coverage '

--- aguamenti/os_utils.py
+++ aguamenti/os_utils.py
@@ -1,6 +1,14 @@
 import os
 
 
+unicode_usa = 'en_US.utf-8'
+os.environ['LC_LANG'] = unicode_usa
+os.environ["LC_ALL"] = unicode_usa
+
+HOME = os.path.expanduser("~")
+REFLOW_WORKFLOWS = os.path.join(HOME, "reflow-workflows")
+
+
 def sanitize_path(path):
     """Make sure path is absolute and user-expanded"""
     return os.path.abspath(os.path.expanduser(path))
@@ -10,13 +18,3 @@ def maybe_add_slash(path):
     """Add a final trailing slash if it wasn't there already"""
     with_trailing_slash = path if path.endswith('/') else path + '/'
     return with_trailing_slash
-
-
-@property
-def HOME():
-    return os.path.expanduser("~")
-
-
-@property
-def REFLOW_WORKFLOWS():
-    return os.path.join(HOME, "reflow-workflows")
--- aguamenti/tests/test_rnaseq.py
+++ aguamenti/tests/test_rnaseq.py
@@ -3,20 +3,60 @@
 import os
 
 from click.testing import CliRunner
+import pytest
+import pandas as pd
+import pandas.testing as pdt
 
 
-def test_rnaseq(data_folder):
+@pytest.fixture
+def experiment_id():
+    return '20181030_FS10000331_12_BNT40322-1214'
+
+
+@pytest.fixture
+def taxon():
+    return 'mus'
+
+
+@pytest.fixture
+def s3_output():
+    return 's3://olgabot-maca/aguamenti-test/'
+
+
+def test_rnaseq(data_folder, experiment_id, taxon, s3_output):
     from aguamenti.rnaseq import align
 
-    experiment_id = '20181030_FS10000331_12_BNT40322-1214'
-    taxon = 'mus'
-    s3_output = 's3://olgabot-maca/aguamenti-test/'
-
     csv = os.path.join(data_folder, 'rnaseq_align.csv')
-    with open(csv) as f:
-        true = f.read()
+    true = pd.read_csv(csv)
 
     runner = CliRunner()
     result = runner.invoke(align, [experiment_id, taxon, s3_output])
+
+    # exit code of '0' means success!
     assert result.exit_code == 0
-    assert result.output == true
+    assert 'samples.csv' in result.output
+    assert 'config.json' in result.output
+
+    # Make sure the files are there
+    assert os.path.exists('samples.csv')
+    assert os.path.exists('config.json')
+
+    # Ensure file contents are correct
+    test = pd.read_csv('samples.csv')
+    pdt.assert_frame_equal(test, true)
+
+
+
+def test_rnaseq_custom_output(data_folder, experiment_id, taxon, s3_output,
+                              tmpdir):
+    from aguamenti.rnaseq import align
+
+    runner = CliRunner()
+    result = runner.invoke(align, ["--output", tmpdir,
+                                   experiment_id, taxon, s3_output])
+    # exit code of '0' means success!
+    assert result.exit_code == 0
+
+    # Make sure the files are there
+    assert os.path.exists(os.path.join(tmpdir, 'samples.csv'))
+    assert os.path.exists(os.path.join(tmpdir, 'config.json'))
