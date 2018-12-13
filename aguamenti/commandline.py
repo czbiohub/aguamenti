c435af27323d34090a9ca5904e306f5fd8b435a2
Olga Botvinnik <olga.botvinnik@gmail.com>
2018-12-12 16:33:02 -0800

    Add command for a batch test run

--- aguamenti/commandline.py
+++ aguamenti/commandline.py
@@ -6,6 +6,7 @@ from .os_utils import os  # noqa: F401
 import click
 
 from .rnaseq import align
+from .check import check_batch
 from .status import listbatches
 
 settings = dict(help_option_names=['-h', '--help'])
@@ -21,6 +22,7 @@ def cli():
 
 
 cli.add_command(align, name='rnaseq-align')
+cli.add_command(check_batch, name="check-batch")
 cli.add_command(listbatches, name="status")
 
 if __name__ == "__main__":
