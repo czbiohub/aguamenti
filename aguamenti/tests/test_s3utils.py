979a01099f55dc91d7e297bb45e8a27abd3b8eaf
Olga Botvinnik <olga.botvinnik@gmail.com>
2019-03-04 09:22:42 -0800

    Add test for getting _{1,2}.fastq.gzs

---
+++ aguamenti/tests/test_s3utils.py
@@ -0,0 +1,21 @@
+
+import json
+import os
+
+from click.testing import CliRunner
+import pytest
+import pandas as pd
+import pandas.testing as pdt
+
+
+@pytest.fixture
+def sra_output_folder():
+    return "s3://olgabot-maca/sra/danio_rerio/smart-seq/whole_kidney_marrow_prjna393431/"
+
+
+def test_get_fastqs_as_r1_r2_columns(sra_output_folder):
+    from aguamenti.s3_utils import get_fastqs_as_r1_r2_columns
+    fastqs = get_fastqs_as_r1_r2_columns(s3_input_path=sra_output_folder)
+
+    # 246 samples --> 492 reads
+    assert len(fastqs) == 246
